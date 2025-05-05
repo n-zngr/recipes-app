e430c3c52a88a8909b283113f00ac1c83900dcad
n-zngr <deltagaming.ch.nik@gmail.com>
2025-05-05 20:01:07 +0200

    fix(db): remove env credentials
    - remove credentials path from env, due to causing an error

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,8 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
 
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
+cred = credentials.Certificate("./firebase/serviceAccount.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
