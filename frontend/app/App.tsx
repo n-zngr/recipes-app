f7876100c7b35b7b974bfc8119dcd3d986fe8385
n-zngr <deltagaming.ch.nik@gmail.com>
2025-05-06 22:27:30 +0200

    style(app): remove boiler styling, update styling

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,19 +1,19 @@
-import './App.css'
-
 const App: React.FC = () => {
 
     return (
-        <div className='bg-neutral-200 dark:bg-neutral-800'>
-            <h1>Ingredients & Recipes</h1>
-            <ul>
-                <h2>Ingredients</h2>
-                <div>
+        <div className='h-dvh w-screen bg-neutral-800 text-white p-4'>
+            <h1 className="mb-4">Ingredients & Recipes</h1>
+            <div className="flex">
+                <div className="flex flex-col bg-neutral-700 w-64 p-4">
+                    <h2>Ingredients</h2>
+                    <div className="">
 
+                    </div>
                 </div>
-            </ul>
-            <ul>
-                <h2>Recipes</h2>
-            </ul>
+                <div className="">
+                    Recipes
+                </div>
+            </div>
         </div>
     )
 }
